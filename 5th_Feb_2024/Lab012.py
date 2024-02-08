9a509267ccfd759a5fbfe49df4e26efe7eca8619
prititimande <158988992+prititimande@users.noreply.github.com>
2024-02-09 00:15:00 +0530

    Hi

---
+++ 5th_Feb_2024/Lab012.py
@@ -0,0 +1,7 @@
+# Take the 2 numbers from user and Add them,
+# Step 1 = Take Input
+# Step 2 = Sum
+num1 = int(input("Enter the 1st Number"))
+num2 = int(input("Enter the 2nd Number"))
+num3 = (num1+num2)
+print(num3)
