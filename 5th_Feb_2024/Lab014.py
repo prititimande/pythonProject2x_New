c50bab9ff26cfdc6c93570e059ee2a988a6a42ae
prititimande <158988992+prititimande@users.noreply.github.com>
2024-02-09 00:40:00 +0530

    Hi

---
+++ 5th_Feb_2024/Lab014.py
@@ -0,0 +1,19 @@
+# Strings
+# Bunch of char
+# "" or ''
+name1 = "Priti"
+name2 = 'Priti'
+print(name1)
+print(name2)
+print(type(name1))
+print(type(name2))
+
+# raw string
+# This will be useful in dir path
+dir = "C:\\abc\\abc.txt"
+print(dir)
+
+# Format String
+first_name = "Amit"
+last_name = "Mishra"
+print(f"Your Name is {first_name} {last_name}")
